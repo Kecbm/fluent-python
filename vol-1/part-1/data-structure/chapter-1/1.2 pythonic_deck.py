794cbece273b1b3e22d4c65dda10d2660897654e
Klecianny Melo <klecianny.melo@quorum.us>
2026-03-11 23:26:20 -0300

    :hammer: remove empty lines

--- vol-1/part-1/data-structure/chapter-1/1.2 pythonic_deck.py
+++ vol-1/part-1/data-structure/chapter-1/1.2 pythonic_deck.py
@@ -15,21 +15,17 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
-
 # Text representation for a card in a deck
 beer_card = Card('7', 'diamonds')
 print(f"{beer_card}") # Card(rank='7', suit='diamonds')
-
 
 # Number of cards in a deck
 deck = FrenchDeck()
 print(f"{len(deck)}") # 52
 
-
 # Read first and last cards
 print(f"{deck[0]}") # Card(rank='2', suit='spades')
 print(f"{deck[-1]}") # Card(rank='A', suit='hearts')
-
 
 # Get a random card
 from random import choice
